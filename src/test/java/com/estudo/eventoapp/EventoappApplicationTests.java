767075bbbbfaec9f9c34f45c413b5fd28800121a
Pedro Henrique <pedrotxc@gmail.com>
2023-04-16 17:45:51 -0300

    Projeto Evento App

---
+++ src/test/java/com/estudo/eventoapp/EventoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.eventoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
