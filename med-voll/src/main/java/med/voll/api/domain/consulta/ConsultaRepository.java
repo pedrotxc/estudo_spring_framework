bfe379fa29961e6fd6e5bc02124296d26e18ee84
Pedro Henrique <pedrotxc@gmail.com>
2024-02-18 00:02:53 -0300

    Atualizando projetos com validacoes

--- med-voll/src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
+++ med-voll/src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -3,6 +3,11 @@ package med.voll.api.domain.consulta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
 @Repository
 public interface ConsultaRepository extends JpaRepository<Consulta,Long> {
+    Boolean existsByMedicoIdAndData(Long dados);
+
+    Boolean existsByPacienteIdAndDataBetween(Long aLong, LocalDateTime primeiroHorario, LocalDateTime ultimoHorario);
 }
--- med-voll/src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
+++ med-voll/src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
@@ -1,8 +1,17 @@
 package med.voll.api.domain.paciente;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+
+    @Query("""
+            select p.ativo
+            from Paciente p
+            where 
+            p.id = :id
+            """)
+    Boolean findAtivoById(Long id);
 }
